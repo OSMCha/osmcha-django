8eb4be588187577da93e6bc6de85010cb819fa34
Wille Marcel <wille@wille.blog.br>
2015-09-03 21:30:31 -0300

    set rabbitmq as broker and install celery

--- config/settings/production.py
+++ config/settings/production.py
@@ -124,14 +124,9 @@ DATABASES['default'] = env.db("DATABASE_URL")
 # ------------------------------------------------------------------------------
 # Heroku URL does not pass the DB number, so we parse it in
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "{0}/{1}".format(env.cache_url('REDIS_URL', default="redis://127.0.0.1:6379"), 0),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "IGNORE_EXCEPTIONS": True,  # mimics memcache behavior.
-                                        # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
-        }
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': ''
     }
 }
 
