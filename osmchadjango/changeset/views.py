fb9562e354171d4b1c8d5eeb92925b5784dc2b46
Wille Marcel <wille@wille.blog.br>
2015-09-03 23:35:43 -0300

    improve changeset_list template

--- osmchadjango/changeset/views.py
+++ osmchadjango/changeset/views.py
@@ -5,6 +5,6 @@ from .models import Changeset
 
 class ChangesetListView(ListView):
     """List Changesets"""
-    queryset = Changeset.objects.all().order_by('date')
+    queryset = Changeset.objects.all().order_by('-date')
     context_object_name = 'changesets'
     paginate_by = 15
