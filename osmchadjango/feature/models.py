f2a1290686e430af515ba2133694c25520e9fe5d
amishas157 <amishas157@gmail.com>
2016-10-14 15:33:52 +0530

    Add diff tables

--- osmchadjango/feature/models.py
+++ osmchadjango/feature/models.py
@@ -4,6 +4,7 @@ from django.contrib.postgres.fields import JSONField
 
 from ..users.models import User
 import json
+import codecs
 
 class Feature(models.Model):
 
@@ -23,6 +24,34 @@ class Feature(models.Model):
     def __str__(self):
         return '%s' % self.id
 
+    @classmethod
+    def decode_string(value):
+        return codecs.decode(value, 'unicode-escape')
+
     @property
     def geojson_obj(self):
         return json.loads((self.geojson).replace('osm:', 'osm_'))
+
+    @property
+    def diff_tags(self):
+        geojson = json.loads((self.geojson))
+        modified_tags = []
+        deleted_tags = []
+        tags = {}
+        for key, value in geojson['properties']['oldVersion']['properties'].iteritems():
+            if key in geojson['properties']:
+                if value != geojson['properties'][key]:
+                    record = {}
+                    record["tag"] = key
+                    record["oldValue"] = value
+                    record["newValue"] = geojson['properties'][key]
+                    modified_tags.append(record)
+            else:
+                record = {}
+                record["tag"] = key
+                record["Value"] =  value
+                deleted_tags.append(record)
+
+        tags["modified"] = modified_tags
+        tags["deleted"] = deleted_tags
+        return tags
