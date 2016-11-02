301fa67deef87cc4f6011d385fd06bbe81b2082d
amishas157 <amishas157@gmail.com>
2016-11-02 16:29:59 +0530

    Remove user meta data

--- osmchadjango/changeset/filters.py
+++ osmchadjango/changeset/filters.py
@@ -6,7 +6,6 @@ from django_filters import filters
 class ChangesetFilter(django_filters.FilterSet):
 
     max_score = filters.MethodFilter()
-    max_user_score = filters.MethodFilter()
     checked = filters.MethodFilter()
     harmful = filters.MethodFilter()
     is_suspect = filters.MethodFilter()
@@ -15,11 +14,6 @@ class ChangesetFilter(django_filters.FilterSet):
     def filter_max_score(self, queryset, value):
         if value:
             return queryset.filter(score__lte=value).exclude(score__isnull=True)
-        return queryset
-
-    def filter_max_user_score(self, queryset, value):
-        if value:
-            return queryset.filter(user_detail__score__lte=value).exclude(user_detail__score__isnull=True)
         return queryset
 
     def filter_checked(self, queryset, value):
@@ -54,10 +48,6 @@ class ChangesetFilter(django_filters.FilterSet):
             'delete': ['gte', 'lte'],
             'date': ['gte', 'lte'],
             'max_score': [],
-            'max_user_score': [],
-            'user_detail__score': ['lte'],
-            'user_detail__changesets_no': ['lte'],
-            'user_detail__changesets_changes': ['lte'],
             'editor': ['icontains'],
             'comment': ['icontains'],
             'source': ['icontains'],
