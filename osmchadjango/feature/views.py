292f1d45fc850804a2aea7eaaa0d3dde97209f73
amishas157 <amishas157@gmail.com>
2016-11-21 11:37:43 +0530

    Merge

--- osmchadjango/feature/views.py
+++ osmchadjango/feature/views.py
@@ -150,7 +150,6 @@ def suspicion_create(request):
             except:
                 suspicious_feature.oldGeometry = None
             suspicious_feature.oldGeojson= json.dumps(feature['properties'].pop("oldVersion"))
-        suspicious_feature.geometry = GEOSGeometry(json.dumps(feature['geometry']))
         suspicious_feature.geojson = json.dumps(feature)
         suspicious_feature.comparator_version = feature.get('comparator_version')
         suspicious_feature.url = suspicious_feature.osm_type + '-' + str(suspicious_feature.osm_id)
