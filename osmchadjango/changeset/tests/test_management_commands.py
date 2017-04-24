7dd5e928a8b94c5db856bbc20adeed270c648a71
Wille Marcel <wille@wille.blog.br>
2017-04-24 10:48:19 -0300

    remove generate_harmful_csv command and django-queryset-csv lib

--- osmchadjango/changeset/tests/test_management_commands.py
+++ osmchadjango/changeset/tests/test_management_commands.py
@@ -1,39 +1,9 @@
-from tempfile import mkdtemp
-from os.path import join, exists
-from shutil import rmtree
-from csv import reader
-
 from django.conf import settings
 from django.core.management import call_command
 from django.test import TestCase
 from django.utils.six import StringIO
 
-from .modelfactories import HarmfulChangesetFactory, SuspectChangesetFactory
 from ..models import Changeset
-
-
-class TestHarmfulCSV(TestCase):
-    def setUp(self):
-        self.dir = mkdtemp()
-        self.csv = join(self.dir, 'test.csv')
-        SuspectChangesetFactory()
-        self.changeset = HarmfulChangesetFactory()
-        self.out = StringIO()
-        call_command('generate_harmful_csv', self.csv, stdout=self.out)
-
-    def test_csv_creation(self):
-        self.assertTrue(exists(self.csv))
-        csv = reader(open(self.csv, 'r'))
-        csv_rows = [row for row in csv]
-        self.assertEqual(len(csv_rows), 2)
-        self.assertEqual(csv_rows[1][0], str(self.changeset.id))
-        self.assertIn(
-            'File {} created.'.format(self.csv),
-            self.out.getvalue()
-            )
-
-    def tearDown(self):
-        rmtree(self.dir)
 
 
 class TestImportReplicationFile(TestCase):
