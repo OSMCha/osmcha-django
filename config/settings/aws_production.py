18dfb4a65465174a904967893ad077ac0ddf5c93
Wille Marcel <wille@wille.blog.br>
2017-08-09 07:43:18 -0300

    disable SessionAuthentication in DRF endpoints

--- config/settings/aws_production.py
+++ config/settings/aws_production.py
@@ -66,7 +66,6 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
