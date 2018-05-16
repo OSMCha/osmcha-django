9f61ac28d5991c8cc8c11aa7fd9b2dfbfb02deb3
Wille Marcel <wille@wille.blog.br>
2018-05-16 06:37:13 -0300

    fix error in url

--- osmchadjango/frontend/urls.py
+++ osmchadjango/frontend/urls.py
@@ -3,8 +3,6 @@ from __future__ import absolute_import, unicode_literals
 from django.urls import re_path
 from django.views.generic.base import TemplateView, RedirectView
 
-from . import views
-
 
 app_name = 'frontend'
 urlpatterns = [
