7e1798a49f82133be7546763a65b00c130579994
Wille Marcel <wille.yyz@gmail.com>
2020-01-05 12:40:59 -0300

    update django to 2.2, drf to 3.10 and drf-gis to 0.15

--- config/settings/common.py
+++ config/settings/common.py
@@ -331,6 +331,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
     'DEFAULT_FILTER_BACKENDS': (
